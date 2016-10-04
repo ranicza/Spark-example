601c90dc02c448a58a83e648876ee1adbc3b6fae
Maryna Maroz <maryna_maroz@epam.com>
2016-10-04 12:49:12 +0300

    draft

--- src/main/java/com/epam/bigdata/q3/task8/SparkUniqueWords.java
+++ src/main/java/com/epam/bigdata/q3/task8/SparkUniqueWords.java
@@ -5,53 +5,102 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FlatMapFunction;
-import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.api.java.function.Function;
 
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+
+import java.util.Date;
+
 import java.util.regex.Pattern;
-import scala.Tuple2;
 
-import java.lang.Iterable;
+import java.text.SimpleDateFormat;
 
 public class SparkUniqueWords {
-	private static final Pattern SPACE = Pattern.compile(" ");
+	private static final String SPLIT = "\\s+";
+	private static final String FORMAT = "yyyyMMdd";
+	
+	
+	//private static final Pattern SPACE = Pattern.compile(" ");
 	private static final String INPUT = "hdfs://sandbox.hortonworks.com:8020/tmp/admin/stream.20130607-ak.txt";
-	private static final String OUTPUT = "hdfs://sandbox.hortonworks.com:8020/tmp/admin/out.txt";
+	private static final String OUTPUT = "hdfs://sandbox.hortonworks.com:8020/tmp/admin/out1.txt";
+	
+	//private static final String TAB = "\\t";
 
-	/*
-	  public static void main(String[] args) throws Exception {
-//		String inputFile = args[0];
-//	    String outputFile = args[1];
-		  
-			String inputFile = INPUT;
-		    String outputFile = OUTPUT;
-		  
-	    
+	
+	//private static final String INPUT = "fd46dc0f95eecaa17e223dab98268f57	20130607213331598	Vh1DCic73QFd3oL	Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; Sicent; WoShiHoney.B; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)	58.210.120.*	80	85	3	31drTvprdN1RFt	51bdec5216ef68820cfd3b2077a7a6b7	null	Edu_F_Upright	300	250	0	0	20	a499988a822facd86dd0e8e4ffef8532	300	1458	289291300775	0"+
+//"ce58cba59ec01e66400c233d74c0ce4e	20130607213331602	VhK2CkN3PZF6Job	Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1	14.134.152.*	368	371	3	DD1SqS9rg5scFsf	46d439e5e082c02cf982adeaac8f07c	null	Auto_F_Rectangle	300	250	0	0	50	e1af08818a6cd6bbba118bb54a651961	254	3476	282825712806	1";
+	 
+	
+	public static void main(String[] args) throws Exception {
+		String inputFile = args[0];
+	    String outputFile = args[1];
+
 	    if (args.length < 1) {
 	      System.err.println("Usage: file <file_input>  <file_output>");
 	      System.exit(1);
-	    }
+	    }    
+	    
+	    SparkSession spark = SparkSession
+	    	      .builder()
+	    	      .appName("SparkUniqueWords")
+	    	      .getOrCreate();
+
+        JavaRDD<ULogEntity> logEntities = spark.read().text(INPUT).javaRDD().map(new Function<String, ULogEntity>() {
+            @Override
+            public ULogEntity call(String line) throws Exception {
+                String[] parts = line.split(SPLIT);
+
+                ULogEntity logEntity = new ULogEntity();
+                logEntity.setIDUserTags(Integer.parseInt(parts[parts.length-2]));
+                logEntity.setIDCity(Integer.parseInt(parts[parts.length-15]));
+
+                SimpleDateFormat formatter = new SimpleDateFormat(FORMAT);
+                String strDate = parts[1].substring(0,8);
+                Date date = formatter.parse(strDate);
+                logEntity.setTmsDate(date);
+                return logEntity;
+            }
+        });
+        
+        logEntities.saveAsTextFile(OUTPUT);
+        System.out.println();
+	    
+//        JavaRDD<ULogEntity> logEntities = spark.read().text(inputFile).javaRDD().map(new Function<String, ULogEntity>() {
+//            @Override
+//            public ULogEntity call(String line) throws Exception {
+//                String[] parts = line.split(SPLIT);
+//
+//                ULogEntity logEntity = new ULogEntity();
+//                logEntity.setIDUserTags(Integer.parseInt(parts[parts.length-2]));
+//                logEntity.setIDCity(Integer.parseInt(parts[parts.length-15]));
+//
+//                SimpleDateFormat formatter = new SimpleDateFormat(FORMAT);
+//                String strDate = parts[1].substring(0,8);
+//                Date date = formatter.parse(strDate);
+//                logEntity.setTmsDate(date);
+//                return logEntity;
+//            }
+//        });
+        
+       
+        
+
+	    
 
 //	    SparkConf conf = new SparkConf().setMaster("local[8]").setAppName("uniqueWords");
-	    SparkConf conf = new SparkConf().setAppName("uniqueWords");
-	    JavaSparkContext sc = new JavaSparkContext(conf);
-	    JavaRDD<String> lines = sc.textFile(inputFile);
-	    lines.cache();
-	    
-	    
-	    
-	    
-
+//	    SparkConf conf = new SparkConf().setAppName("uniqueWords");
+//	    JavaSparkContext sc = new JavaSparkContext(conf);    
+//	    
+//	    JavaRDD<String> lines = sc.textFile(inputFile);
+//	    lines.cache();
+/*
 		 //Get separate params from row
 		 JavaRDD<String> params = lines.flatMap(new FlatMapFunction<String, String>() {
 		      @Override
 		      public Iterator<String> call(String s) {
-		    	  System.out.println(Arrays.asList(SPACE.split(s)).iterator());
-		        return Arrays.asList(SPACE.split(s)).iterator();
+		    	  System.out.println(Arrays.asList(TAB.split(s)).iterator());
+		        return Arrays.asList(TAB.split(s)).iterator();
 		      }
 		   });
 		 
@@ -71,50 +120,8 @@ public class SparkUniqueWords {
 		            public Integer call(Integer x, Integer y){ return x + y;}});
 		    // Save the word count back out to a text file, causing evaluation.
 		    counts.saveAsTextFile(outputFile);	
+		    */
 	  }
-	*/  
-	  public static void main(String[] args) throws Exception {
 
-	        if (args.length < 1) {
-	            System.err.println("Usage: JavaWordCount <file>");
-	            System.exit(1);
-	        }
 
-	        SparkSession spark = SparkSession
-	                .builder()
-	                .appName("JavaWordCount")
-	                .getOrCreate();
-	        
-	        
-	        JavaRDD<String> lines = spark.read().text(args[0]).javaRDD();
-
-	        JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
-	            @Override
-	            public Iterator<String> call(String s) {
-	                return Arrays.asList(SPACE.split(s)).iterator();
-	            }
-	        });
-
-	        JavaPairRDD<String, Integer> ones = words.mapToPair(
-	                new PairFunction<String, String, Integer>() {
-	                    @Override
-	                    public Tuple2<String, Integer> call(String s) {
-	                        return new Tuple2<>(s, 1);
-	                    }
-	                });
-
-	        JavaPairRDD<String, Integer> counts = ones.reduceByKey(
-	                new Function2<Integer, Integer, Integer>() {
-	                    @Override
-	                    public Integer call(Integer i1, Integer i2) {
-	                        return i1 + i2;
-	                    }
-	                });
-
-	        List<Tuple2<String, Integer>> output = counts.collect();
-	        for (Tuple2<?,?> tuple : output) {
-	            System.out.println(tuple._1() + ": " + tuple._2());
-	        }
-	        spark.stop();
-	    }
 }
---
+++ src/main/java/com/epam/bigdata/q3/task8/ULogEntity.java
@@ -0,0 +1,73 @@
+package com.epam.bigdata.q3.task8;
+
+import java.util.Date;
+
+public class ULogEntity {
+	
+	private int  IDUserTags;
+	
+	private int IDCity;
+	
+	private Date tmsDate;
+
+	public ULogEntity() {
+	}
+
+	public int getIDUserTags() {
+		return IDUserTags;
+	}
+
+	public void setIDUserTags(int iDUserTags) {
+		IDUserTags = iDUserTags;
+	}
+
+	public int getIDCity() {
+		return IDCity;
+	}
+
+	public void setIDCity(int iDCity) {
+		IDCity = iDCity;
+	}
+
+	public Date getTmsDate() {
+		return tmsDate;
+	}
+
+	public void setTmsDate(Date tmsDate) {
+		this.tmsDate = tmsDate;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + IDCity;
+		result = prime * result + IDUserTags;
+		result = prime * result + ((tmsDate == null) ? 0 : tmsDate.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ULogEntity other = (ULogEntity) obj;
+		if (IDCity != other.IDCity)
+			return false;
+		if (IDUserTags != other.IDUserTags)
+			return false;
+		if (tmsDate == null) {
+			if (other.tmsDate != null)
+				return false;
+		} else if (!tmsDate.equals(other.tmsDate))
+			return false;
+		return true;
+	}
+
+	
+	
+}
